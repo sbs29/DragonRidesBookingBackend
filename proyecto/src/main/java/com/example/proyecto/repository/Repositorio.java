1c4fdd9725517a6d7173e71ee083440628d86ccb
Hcarfon <49518996+Hcarfon@users.noreply.github.com>
2019-04-23 10:42:30 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/sbs29/DragonRidesBookingBackend""
    This reverts commit 04c5a1866d373d77ca5671a445f05ec828213d52.

---
+++ proyecto/src/main/java/com/example/proyecto/repository/Repositorio.java
@@ -0,0 +1,9 @@
+package com.example.proyecto.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.proyecto.CredencialesLogin;
+
+public interface Repositorio extends CrudRepository<CredencialesLogin, Long>{
+
+}
