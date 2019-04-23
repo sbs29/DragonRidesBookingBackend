c7b80773b25c346feebb4f6b9133948348a0a4fd
Miguel <49518849+mfernandezcarnota@users.noreply.github.com>
2019-04-23 12:32:38 +0200

    Add files via upload

---
+++ proyecto/src/main/java/entities/Airlines.java
@@ -0,0 +1,47 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Airlines {
+	
+	@Id
+	private Long AirlineId;
+
+	@Column
+	private String AirlineName;
+	
+	@Column
+	private String AirlineImage;
+	
+	public Airlines() {
+	}
+
+	public void setAirlineId(Long AirlineId) {
+		this.AirlineId = AirlineId;
+	}
+
+	public String getAirlineName() {
+		return AirlineName;
+	}
+
+	public void setAirlineName(String AirlineName) {
+		this.AirlineName = AirlineName;
+	}
+
+	public String getAirlineImage() {
+		return AirlineImage;
+	}
+
+	public void setAirlineImage(String AirlineImage) {
+		this.AirlineImage = AirlineImage;
+	}
+
+	@Override
+	public String toString() {
+		return "Airlines [AirlineId=" + AirlineId + ", AirlineName=" + AirlineName + ", AirlineImage=" + AirlineImage + "]";
+	}
+	
+}
