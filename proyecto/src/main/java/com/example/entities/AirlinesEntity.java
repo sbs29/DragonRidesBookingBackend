3a9838f5b5db5e069baa0eed01e4a8659b135a5d
Miguel <49518849+mfernandezcarnota@users.noreply.github.com>
2019-04-24 10:30:01 +0200

    Add files via upload

---
+++ proyecto/src/main/java/com/example/entities/AirlinesEntity.java
@@ -0,0 +1,48 @@
+package com.example.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class AirlinesEntity extends AbstractEntity {
+
+	@Id
+	private Long id;
+	@Column
+	private String airlineName;
+	@Column
+	private String airlineImage;
+	
+	public AirlinesEntity() {}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAilineName() {
+		return airlineName;
+	}
+
+	public void setAilineName(String airlineName) {
+		this.airlineName = airlineName;
+	}
+
+	public String getAirlineImage() {
+		return airlineImage;
+	}
+
+	public void setAirlineImage(String airlineImage) {
+		this.airlineImage = airlineImage;
+	}
+
+	@Override
+	public String toString() {
+		return "Airlines [id=" + id + ", airlineName=" + airlineName + ", airlineImage=" + airlineImage + "]";
+	}
+	
+}
---
+++ proyecto/src/main/java/com/example/entities/DestinationsEntity.java
@@ -0,0 +1,38 @@
+package com.example.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class DestinationsEntity extends AbstractEntity {
+
+	@Id
+	private Long id;
+	@Column
+	private String name;
+	
+	public DestinationsEntity() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Destinations [id=" + id + ", name=" + name + "]";
+	}
+	
+}
