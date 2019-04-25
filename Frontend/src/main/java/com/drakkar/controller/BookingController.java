ed1c65e5ebf04eef6babf10f7fcf9bffb526b65f
sergitovazquez <sergitovazquez96@gmail.com>
2019-04-25 10:17:17 +0200

    cambios

--- Frontend/src/main/java/com/drakkar/controller/BookingController.java
+++ Frontend/src/main/java/com/drakkar/controller/BookingController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.drakkar.controller;
 
 import java.util.Map;
@@ -16,26 +16,4 @@ public class BookingController {
 
 }
 }
-=======
-package com.drakkar.controller;
 
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class BookingController {
-
-	@RequestMapping("/Booking")
-	public String booking(Map<String, Object> model) {
-<<<<<<< HEAD
-		return "PaginaBooking";
-=======
-		return "Booking";
->>>>>>> 5e151b7cd0d0ed8a09e69473d77583ea641f9688
-
-}
-}
->>>>>>> e4cb6a1b98cefb54edccbac65e7fb1389b6d5c8c
--- Frontend/src/main/java/com/drakkar/controller/SearchController.java
+++ Frontend/src/main/java/com/drakkar/controller/SearchController.java
@@ -8,19 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SearchController {
 
-	
-	
-<<<<<<< HEAD:Frontend/src/main/java/com/drakkar/controller/BusquedaController.java
-		@RequestMapping("/Busqueda")
-		public String Busqueda(Map<String, Object> model) {
-			return "Busqueda";
-=======
-		@RequestMapping("/Search")
-		public String search(Map<String, Object> model) {
-			return "Search";
->>>>>>> 5e151b7cd0d0ed8a09e69473d77583ea641f9688:Frontend/src/main/java/com/drakkar/controller/SearchController.java
-
+	@RequestMapping("/Search")
+	public String search(Map<String, Object> model) {
+		return "Search";
 	}
-	}
+}
 
 
