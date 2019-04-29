062b8062b9343c0174cbca0b34617034d4b054a2
eduyayo <efernandezq@GO040148.itdeusto.local>
2019-04-29 16:09:36 +0200

    implemento el servicio de destinos

--- Frontend/src/main/java/com/drakkar/service/DestinationService.java
+++ Frontend/src/main/java/com/drakkar/service/DestinationService.java
@@ -1,9 +1,12 @@
 package com.drakkar.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.drakkar.domain.Destination;
 
@@ -11,17 +14,14 @@ import com.drakkar.domain.Destination;
 public class DestinationService {
 
 	public List<Destination> findAll() {
-		//TODO get this from a remote REST call
-		List<Destination> destinations = new ArrayList<Destination>();
-		Destination e = new Destination();
-		e.setId(32L);
-		e.setName("nombre 32");
-		destinations.add(e);
-		e = new Destination();
-		e.setId(45L);
-		e.setName("cuarenta y cinco");
-		destinations.add(e);
-		return destinations;
-	}
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Destination>> response = restTemplate.exchange(
+        		  "http://localhost:8080/destination/", //TODO get the url from the app configuration.
+        		  HttpMethod.GET,
+        		  null,
+        		  new ParameterizedTypeReference<List<Destination>>(){});
+        return response.getBody();
+    }
 
 }
